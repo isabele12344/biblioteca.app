f57b0e3946f1609e71c946fec600cf484f276819
Your Name <you@example.com>
2025-07-11 10:57:22 -0300

    adionado rodape e cabecalho

---
+++ src/lib/componentes/Menu.svelte
@@ -0,0 +1,8 @@
+<div>
+    <h1>Biblioteca</h1>
+    <nav>
+        <li>
+            Livros
+        </li>
+    </nav>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import Menu from '$lib/componentes/Menu.svelte';
+	import Rodape from '$lib/componentes/Rodape.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
-
+<Menu/>
 {@render children()}
+<Rodape/>
