866a0c3a9c535cec671fd3b1f79e45bae9d62d28
Pedro Kauã Chaves Sotero <soteropedro84@gmail.com>
2025-08-06 22:12:36 -0300

    ultima atualização

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
     import Header from '$lib/componentes/Header.svelte';
     import Footer from '$lib/componentes/Footer.svelte';
+    import { goto } from '$app/navigation';
 </script>
-
 <Header />
+<nav class="botão">
+    <button on:click={() => goto('/')}>Livros mais vendidos</button>
+</nav>
 <slot />
 <Footer />
+<style>
+    .botão{
+        text-align: center;
+        margin: 1rem 0;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="br">
 	import Destaques from "$lib/componentes/Destaques.svelte";
 
-     
+    const livros = [
+        { titulo: "A Menina que Roubava Livros", autor: "Markus Zusak" },
+        { titulo: "O Alquimista", autor: "Paulo Coelho" },
+        { titulo: "Dom Casmurro", autor: "Machado de Assis" }
+    ];
 </script>
 
 <Destaques/>
+
+<h1>Livros Mais Vendidos</h1>
+<ul>
+    {#each livros as livro}
+        <li>
+            <strong>{livro.titulo}</strong> — {livro.autor}
+        </li>
+    {/each}
+</ul>
 
 
